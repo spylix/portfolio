d88940d55230bd6e8f4ae274105bdac7e1698a7c
spylix <pawel66556655@gmail.com>
2021-10-17 17:08:35 +0200

    deployment commit

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,16 @@
-var ghpages = require('gh-pages');
+import { publish } from "gh-pages";
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
-        user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
-        }
+publish(
+  "public", // path to public directory
+  {
+    branch: "gh-pages",
+    repo: "https://github.com/username/yourproject.git", // Update to point to your repository
+    user: {
+      name: "Your name", // update to use your name
+      email: "Your Email address", // Update to use your email
     },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+  },
+  () => {
+    console.log("Deploy Complete!");
+  }
+);
